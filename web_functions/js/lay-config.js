77fb09a0c455fa761ba8879b08a431f839241cbb
Stern188 <769999398@qq.com>
2021-07-21 17:34:12 +0800

    修改引用地址

--- web_functions/js/lay-config.js
+++ web_functions/js/lay-config.js
@@ -6,8 +6,12 @@
  */
 
 window.rootPath = (function (src) {
-    src = document.scripts[document.scripts.length - 1].src;
-    return src.substring(0, src.lastIndexOf("/") + 1);
+    if(location.host.indexOf('github.io')>=0){
+        return location.origin + '/web_functions/js/';
+    }else{
+        src = document.scripts[document.scripts.length - 1].src;
+        return src.substring(0, src.lastIndexOf("/") + 1);
+    }
 })();
 
 layui.config({
